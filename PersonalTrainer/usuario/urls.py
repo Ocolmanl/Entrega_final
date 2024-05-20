2b709cb4ad2a46af347ca64fecd32fbe4e8af677
Colman Sebastian <s.colman.style@gmail.com>
2024-05-20 02:17:29 -0300

    feat: cargando actualizaciones

--- PersonalTrainer/usuario/urls.py
+++ PersonalTrainer/usuario/urls.py
@@ -6,8 +6,11 @@ app_name = 'usuario'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('alumnos/list', views.alumnos_list, name='alumnos_list'),
-    path('alumnos/create', views.alumnos_create, name='alumnos_create'),
-    path('alumnos/delete/<int:pk>', views.alumnos_delete, name='alumnos_delete'),
-    path('alumnos/rutina', views.rutina_create, name='rutina_create'),
+    path('alumno/list', views.alumnos_list, name='alumnos_list'),
+    path('alumno/update/<int:pk>', views.alumnos_update, name='alumnos_update'),
+    path('alumno/delete/<int:pk>', views.alumnos_delete, name='alumnos_delete'),
+    path('alumno/create', views.alumnos_create, name='alumnos_create'),
+    path('rutina/main', views.rutina_main, name='rutina_main'),
+    path('rutina/list', views.rutina_list, name='rutina_list'),
+    path('alumno/rutina', views.rutina_create, name='rutina_create'),
 ]
--- PersonalTrainer/usuario/views.py
+++ PersonalTrainer/usuario/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from usuario.forms import AlumnoCreateForm, RutinaCreateForm
-from usuario.models import Alumno
+from usuario.models import Alumno, Rutina
 
 # Create your views here.
 def index(request):
@@ -15,8 +15,25 @@ def alumnos_list(request):
     else:
         consulta = Alumno.objects.all()
     contexto = {"alumnos": consulta}
-    return render(request, "usuario/consulta_alumnos.html", contexto)
+    return render(request, "usuario/alumnos_list.html", contexto)
 
+def alumnos_update(request, pk: int):
+    consulta = Alumno.objects.get(id=pk)
+    if request.method == "POST":
+        form = Alumno(request.POST, instance=consulta)
+        if form.is_valid():
+            form.save()
+            return redirect("usuario/alumnos_list.html")
+    else:  # GET
+        form = Alumno(instance=consulta)
+    return render(request, "usuario/alumnos_form.html", {"form": form})
+
+def alumnos_delete(request, pk: int):
+    consulta = Alumno.objects.get(id=pk)
+    if request.method == "POST":
+        consulta.delete()
+        return redirect("usuario:consulta_alumnos.html")
+    return render(request, "usuario/confirm_delete_alumno.html", {"object": consulta})
 def alumnos_create(request):
     if request.method == "POST":
         form = AlumnoCreateForm(request.POST)
@@ -25,14 +42,7 @@ def alumnos_create(request):
             return redirect("usuario:index")
     else:  # GET
         form = AlumnoCreateForm()
-    return render(request, "usuario/create_alumnos.html", {"form": form})
-
-def alumnos_delete(request, pk: int):
-    consulta = Alumno.objects.get(id=pk)
-    if request.method == "POST":
-        consulta.delete()
-        return redirect("usuario:consulta_alumnos.html")
-    return render(request, "usuario/confirm_delete_alumno.html", {"object": consulta})
+    return render(request, "usuario/alumnos_form.html", {"form": form})
 
 def rutina_create(request):
     if request.method == "POST":
@@ -42,4 +52,17 @@ def rutina_create(request):
             return redirect("usuario:index")
     else:  # GET
         form = RutinaCreateForm()
-    return render(request, "usuario/rutina_alumno.html", {"form": form})
+    return render(request, "usuario/rutina_create.html", {"form": form})
+
+def rutina_list(request):
+    busqueda = request.GET.get("busqueda", None)
+    if busqueda:
+        print(busqueda)
+        consulta = Rutina.objects.filter(nombre__icontains=busqueda)
+    else:
+        consulta = Rutina.objects.all()
+    contexto = {"rutinas": consulta}
+    return render(request, "usuario/rutina_list.html", contexto)
+
+def rutina_main(request):
+    return render(request, 'usuario/rutina_main.html')
