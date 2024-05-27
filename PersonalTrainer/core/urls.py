699a48048d9913dca5fdfcdc2a0e8a4803b08055
Colman Sebastian <s.colman.style@gmail.com>
2024-05-27 19:25:21 -0300

    feat(logIn LogOut): URL+VIEWS+HTML+FORM

--- PersonalTrainer/core/urls.py
+++ PersonalTrainer/core/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 
-from . import views 
+from django.contrib.auth.views import LogoutView
+
+from . import views
+
+from core.views import CustomLoginView 
 
 app_name = 'core'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login', CustomLoginView.as_view(), name="login"),
+    path('logout/', LogoutView.as_view(template_name='core/logout.html'), name='logout'),
 ]
