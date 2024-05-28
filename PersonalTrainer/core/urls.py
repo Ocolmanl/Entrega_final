671359fe37a326ef87b3137b46e9364084271561
Colman Sebastian <s.colman.style@gmail.com>
2024-05-28 02:59:25 -0300

    feat(register): incorporacion de registro de usuarios

--- PersonalTrainer/core/urls.py
+++ PersonalTrainer/core/urls.py
@@ -4,13 +4,14 @@ from django.contrib.auth.views import LogoutView
 
 from . import views
 
-from core.views import CustomLoginView 
+from core.views import CustomLoginView
 
 app_name = 'core'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('login', CustomLoginView.as_view(), name="login"),
+    path('about/', views.about, name='about'),
+    path('login/', CustomLoginView.as_view(), name="login"),
     path('logout/', LogoutView.as_view(template_name='core/logout.html'), name='logout'),
-    path('aout', views.about, name='about'),
+    path('register/', views.register, name='register'),
 ]
